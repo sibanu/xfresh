bd570cc73db42bb20233c05deb2abc8bb04e048a
sibanu <mrsuteto@gmail.com>
2017-08-16 08:22:32 +0700

    Add files via upload
    setup

---
+++ src/app/fbase.cred.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CRED = {
+    apiKey: "AIzaSyBGKyvs61fb0U8IOsQrLSlCLatkD7Gccn0",
+    authDomain: "test-340dc.firebaseapp.com",
+    databaseURL: "https://test-340dc.firebaseio.com",
+    projectId: "test-340dc",
+    storageBucket: "test-340dc.appspot.com",
+    messagingSenderId: "465322524634"
+  }
